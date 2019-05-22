3b8de03184aad2047dbdbdf1d066fc182bb1458a
Morten Juul <mort79f8@edu.campusvejle.dk>
2019-05-22 21:42:25 +0200

    setup the path added 2 methods GetBoxInfoFromFile and SplittedBoxInfo

--- MortenJuulS1.Reader.Ex2/Program.cs
+++ MortenJuulS1.Reader.Ex2/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace MortenJuulS1.Reader.Ex2
 {
@@ -7,8 +9,32 @@ namespace MortenJuulS1.Reader.Ex2
     {
         static void Main(string[] args)
         {
+            List<Box> boxes = new List<Box>();
+            string path = @"F:\C# - Skole\git\S1MortenJuul\MortenJuulS1.Reader.Ex2\boxInfo.txt";
 
-       
         }
+
+        private static List<string> GetBoxInfoFromFile(string path)
+        {
+            List<string> numbers = new List<string>();
+
+            using (StreamReader reader = new StreamReader(path, Encoding.Default))
+            {
+                string n = "";
+                while ((n = reader.ReadLine()) != null)
+                {
+                    numbers.Add(n);
+                }
+            }
+            return numbers;
+            
+        }
+
+        private static string[] SplitBoxInfo(string input)
+        {
+            string[] splittedInput = input.Split(',');
+            return splittedInput;
+        }
+
     }
 }
