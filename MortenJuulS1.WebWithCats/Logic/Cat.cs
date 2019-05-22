db885aa88f9e449c79c22107c54111f6739add9c
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-05-22 11:47:18 +0200

    created new project added new class called Cat added fields and propperties added a div with class cat, with a h3,img and p.

---
+++ MortenJuulS1.WebWithCats/Logic/Cat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MortenJuulS1.WebWithCats.Logic
+{
+    public class Cat
+    {
+        private string urlToPicture;
+        private string header;
+        private string description;
+        private string altText;
+
+        public string UrlToPicture { get => urlToPicture; set => urlToPicture = value; }
+        public string Header { get => header; set => header = value; }
+        public string Description { get => description; set => description = value; }
+        public string AltText { get => altText; set => altText = value; }
+    }
+}
