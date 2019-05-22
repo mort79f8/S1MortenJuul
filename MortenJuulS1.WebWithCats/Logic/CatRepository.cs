f42bf7bc915e31268f219985a05b1347ba159e8b
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-05-22 13:23:25 +0200

    added some cats to the CatRepository created a loop so each cat would be displayed with all the info. added a styleSheet

---
+++ MortenJuulS1.WebWithCats/Logic/CatRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MortenJuulS1.WebWithCats.Logic
+{
+    public class CatRepository
+    {
+        public List<Cat> GetCatsFromFile()
+        {
+            List<Cat> cats = new List<Cat>();
+            Cat cat1 = new Cat
+            {
+                Header = "Fluffy",
+                Description = "Fluffy is a red cat",
+                AltText = "Pictured of Fluffy the red cat",
+                UrlToPicture = "img/fluffy.jpg"
+            };
+
+            cats.Add(cat1);
+            cats.Add(new Cat
+            {
+                Header = "bunny",
+                Description = "bunny is a grey cat",
+                AltText = "Pictured of bunny the grey cat",
+                UrlToPicture = "img/bunny.jpg"
+            });
+            cats.Add(new Cat
+            {
+                Header = "Mr.Biggles",
+                Description = "Mr.Biggles is a black cat",
+                AltText = "Pictured of Mr.Biggles the black cat",
+                UrlToPicture = "img/mrbiggles.jpg"
+            });
+            return cats;
+        }
+
+    }
+}
