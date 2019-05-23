c6d68027042ecdd2a1e4f65f681f672388322a1b
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-05-23 09:53:08 +0200

    Created new Project MortenJuulS1.Reader.Ex3 Created new class Person added fields for first-/lastname and age added properties for the fields added Constructors for Person Created txt file persons with person data

---
+++ MortenJuulS1.Reader.Ex3/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MortenJuulS1.Reader.Ex3
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int Age { get => age; set => age = value; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, string lastName, int age)
+        {
+            FirstName = name;
+            LastName = lastName;
+            Age = age;
+        }
+    }
+}
